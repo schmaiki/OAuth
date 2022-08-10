b4d38744423b2a1f3a07122c8eecdbe511332e55
Maiki <73941088+schmaiki@users.noreply.github.com>
2022-08-10 16:15:02 +0200

    Index und einstellungen für oauth von github

--- Webserver/main.go
+++ Webserver/main.go
@@ -1,20 +1,126 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/dghubble/gologin"
+	gologin2 "github.com/dghubble/gologin/v2"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/dghubble/gologin/v2/github"
+	"github.com/dghubble/sessions"
+	"golang.org/x/oauth2"
+	githubOAuth2 "golang.org/x/oauth2/github"
 )
 
-func main() {
-	Server := http.NewServeMux()
-	RequestHandler := http.RedirectHandler("http://localhost", 307)
+const (
+	sessionName     = "example-github-app"
+	sessionSecret   = "example cookie signing secret"
+	sessionUserKey  = "githubID"
+	sessionUsername = "githubUsername"
+)
 
-	Server.Handle("/", RequestHandler)
-	log.Print("Server gestartet")
+// sessionStore encodes and decodes session data stored in signed cookies
+var sessionStore = sessions.NewCookieStore([]byte(sessionSecret), nil)
 
-	err := http.ListenAndServe(":5000", Server)
+// Config configures the main ServeMux.
+type Config struct {
+	GithubClientID     string
+	GithubClientSecret string
+}
+
+// New returns a new ServeMux with app routes.
+func New(config *Config) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", profileHandler)
+	mux.HandleFunc("/logout", logoutHandler)
+
+	oauth2Config := &oauth2.Config{
+		ClientID:     config.GithubClientID,
+		ClientSecret: config.GithubClientSecret,
+		RedirectURL:  "http://localhost:3000/github/callback",
+		Endpoint:     githubOAuth2.Endpoint,
+	}
+
+	stateConfig := gologin.DebugOnlyCookieConfig
+	mux.Handle("/github/login", github.StateHandler(gologin2.CookieConfig(stateConfig), github.LoginHandler(oauth2Config, nil)))
+	mux.Handle("/github/callback", github.StateHandler(gologin2.CookieConfig(stateConfig), github.CallbackHandler(oauth2Config, issueSession(), nil)))
+	return mux
+}
+
+func issueSession() http.Handler {
+	fn := func(w http.ResponseWriter, req *http.Request) {
+		ctx := req.Context()
+		githubUser, err := github.UserFromContext(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		session := sessionStore.New(sessionName)
+		session.Values[sessionUserKey] = *githubUser.ID
+		session.Values[sessionUsername] = *githubUser.Login
+		error1 := session.Save(w)
+		if error1 != nil {
+			return
+		}
+		http.Redirect(w, req, "/profile", http.StatusFound)
+	}
+	return http.HandlerFunc(fn)
+}
+
+func profileHandler(w http.ResponseWriter, req *http.Request) {
+	session, err := sessionStore.Get(req, sessionName)
 	if err != nil {
+		// welcome with login button
+		page, _ := os.ReadFile("index.html")
+		_, error1 := fmt.Fprintf(w, string(page))
+		if error1 != nil {
+			return
+		}
 		return
 	}
 
+	_, error2 := fmt.Fprintf(w, `<p>You are logged in %s!</p><form action="/logout" method="post"><input type="submit" value="Logout"></form>`, session.Values[sessionUsername])
+	if error2 != nil {
+		return
+	}
+}
+
+func logoutHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		sessionStore.Destroy(w, sessionName)
+	}
+	http.Redirect(w, req, "/", http.StatusFound)
+}
+
+func main() {
+	const address = "localhost:3000"
+	config := &Config{
+		GithubClientID:     os.Getenv("GITHUB_CLIENT_ID"),
+		GithubClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
+	}
+	clientID := flag.String("client-id", "", "Github Client ID")
+	clientSecret := flag.String("client-secret", "", "Github Client Secret")
+	flag.Parse()
+	if *clientID != "" {
+		config.GithubClientID = *clientID
+	}
+	if *clientSecret != "" {
+		config.GithubClientSecret = *clientSecret
+	}
+	if config.GithubClientID == "" {
+		log.Fatal("Missing Github Client ID")
+	}
+	if config.GithubClientSecret == "" {
+		log.Fatal("Missing Github Client Secret")
+	}
+
+	log.Printf("Starting Server listening on %s\n", address)
+	err := http.ListenAndServe(address, New(config))
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module OAuth
 
 go 1.19
 
-require github.com/dghubble/gologin v2.1.0+incompatible // indirect
+require (
+	github.com/dghubble/gologin v2.1.0+incompatible // indirect
+	github.com/dghubble/gologin/v2 v2.3.0 // indirect
+	github.com/dghubble/sessions v0.2.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/oauth2 v0.0.0-20220808172628-8227340efae7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
